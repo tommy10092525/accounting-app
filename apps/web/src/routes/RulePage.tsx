e4eac6aa0fb56ac915b12c57b35513ab69a0e9d1
tommy10092525 <154877687+tommy10092525@users.noreply.github.com>
2026-09-02 04:49:10 +0900

    利用規約ページを追加

---
+++ apps/web/src/routes/RulePage.tsx
@@ -0,0 +1,27 @@
+import Wrapper from '@/components/Wrapper';
+
+const RulePage = () => {
+  return (
+    <Wrapper>
+      <h1 className="text-2xl font-bold mb-8">利用規約・プライバシーポリシー・特定商取引法に基づく表記</h1>
+      <div className="rounded-md border-2 border-brand-blue bg-card p-4">
+        <div>
+          <h2 className="text-xl my-4 font-bold">利用規約</h2>
+          <p>利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約</p>
+        </div>
+        <div className="w-full border-t border-t-brand-blue m-2"/>
+        <div>
+          <h2 className="text-xl my-4 font-bold">プライバシーポリシー</h2>
+          <p>プライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシープライバシーポリシー</p>
+        </div>
+        <div className="w-full border-t border-t-brand-blue m-2"/>
+        <div>
+          <h2 className="text-xl my-4 font-bold">特定商取引法に基づく表記</h2>
+          <p>特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記特定商取引法に基づく表記</p>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default RulePage
